1808015bc6fcfaba819279fe5e0ecbc9cbb9d095
kalkidanh <kalkidanh89@gmail.com>
2022-09-07 10:26:17 -0700

    putchar added

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef PID_H
+#define PID_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+int _putchar(char c);
+int _strlen(const char *str);
+int tokenize(char *buffer, char *delim);
+char **buff_split(char *buffer, char *delim, int token_count);
+char *copy_buffer(char *buffer);
+
+#endif
