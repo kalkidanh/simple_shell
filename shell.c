ae7a4d8e00da22748473d16adbb6c051bec6995f
kalkidanh <kalkidanh89@gmail.com>
2022-09-08 07:29:41 -0700

    shell complete

--- shell.c
+++ shell.c
@@ -45,3 +45,91 @@ int main(int ac, char **av, char **environ)
 	} while (count != -1);
 	return (0);
 }
+
+/**
+ * _strlen - length of string
+ * @str: string
+ *
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int index = 0;
+
+	while (str[index])
+		index++;
+	return (index);
+}
+
+/**
+ * tokenize - split user input into tokens
+ * @buffer: string to be tokenized
+ * @delim: value of delimiter
+ *
+ * Return: number of tokens
+ */
+int tokenize(char *buffer, char *delim)
+{
+	int token_count;
+	char *token;
+
+	token = strtok(buffer, delim);
+	token_count = 1;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		token_count++;
+	}
+	return (token_count);
+}
+
+/**
+ * buff_split - splits a buffer into an array of tokens
+ * @buffer: string to split
+ * @delim: value of delimiter
+ * @token_count: number of tokens
+ *
+ * Return: array of splitted buffer
+ */
+char **buff_split(char *buffer, char *delim, int token_count)
+{
+	char *token, **argv;
+	int index = 0;
+
+	argv = malloc(sizeof(char *) * token_count);
+	if (argv == NULL)
+		return (NULL);
+	token = strtok(buffer, delim);
+	while (token)
+	{
+		argv[index] = malloc(sizeof(char) * _strlen(token));
+		if (argv[index] == NULL)
+		{
+			free(argv[index]);
+			free(argv);
+			return (NULL);
+		}
+		strcpy(argv[index], token);
+		index++;
+		token = strtok(NULL, delim);
+	}
+	argv[index] = NULL;
+	return (argv);
+}
+
+/**
+ * copy_buffer - copy the content of a buffer
+ * @buffer: string to copy
+ *
+ * Return: a pointer to duplicate buffer
+ */
+char *copy_buffer(char *buffer)
+{
+	char *copy;
+
+	copy = malloc(sizeof(char) * _strlen(buffer));
+	if (copy == NULL)
+		return (NULL);
+	strcpy(copy, buffer);
+	return (copy);
+}
--- shell.s
+++ shell.s
@@ -46,13 +46,13 @@ main:
 .L2:
 	movq	-48(%rbp), %rax
 	movq	%rax, %rdi
-	call	copy_buffer@PLT
+	call	copy_buffer
 	movq	%rax, -24(%rbp)
 	movq	-48(%rbp), %rax
 	movq	-32(%rbp), %rdx
 	movq	%rdx, %rsi
 	movq	%rax, %rdi
-	call	tokenize@PLT
+	call	tokenize
 	movl	%eax, -64(%rbp)
 	movl	-64(%rbp), %eax
 	cltq
@@ -70,7 +70,7 @@ main:
 	movq	-24(%rbp), %rax
 	movq	%rcx, %rsi
 	movq	%rax, %rdi
-	call	buff_split@PLT
+	call	buff_split
 	movq	%rax, -16(%rbp)
 	call	fork@PLT
 	movl	%eax, -60(%rbp)
@@ -108,6 +108,220 @@ main:
 	.cfi_endproc
 .LFE6:
 	.size	main, .-main
+	.globl	_strlen
+	.type	_strlen, @function
+_strlen:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L10
+.L11:
+	addl	$1, -4(%rbp)
+.L10:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L11
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	_strlen, .-_strlen
+	.globl	tokenize
+	.type	tokenize, @function
+tokenize:
+.LFB8:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	-32(%rbp), %rdx
+	movq	-24(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	strtok@PLT
+	movq	%rax, -8(%rbp)
+	movl	$1, -12(%rbp)
+	jmp	.L14
+.L15:
+	movq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$0, %edi
+	call	strtok@PLT
+	movq	%rax, -8(%rbp)
+	addl	$1, -12(%rbp)
+.L14:
+	cmpq	$0, -8(%rbp)
+	jne	.L15
+	movl	-12(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8:
+	.size	tokenize, .-tokenize
+	.globl	buff_split
+	.type	buff_split, @function
+buff_split:
+.LFB9:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$72, %rsp
+	.cfi_offset 3, -24
+	movq	%rdi, -56(%rbp)
+	movq	%rsi, -64(%rbp)
+	movl	%edx, -68(%rbp)
+	movl	$0, -36(%rbp)
+	movl	-68(%rbp), %eax
+	cltq
+	salq	$3, %rax
+	movq	%rax, %rdi
+	call	malloc@PLT
+	movq	%rax, -24(%rbp)
+	cmpq	$0, -24(%rbp)
+	jne	.L18
+	movl	$0, %eax
+	jmp	.L19
+.L18:
+	movq	-64(%rbp), %rdx
+	movq	-56(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	strtok@PLT
+	movq	%rax, -32(%rbp)
+	jmp	.L20
+.L22:
+	movq	-32(%rbp), %rax
+	movq	%rax, %rdi
+	call	_strlen
+	cltq
+	movl	-36(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,8), %rcx
+	movq	-24(%rbp), %rdx
+	leaq	(%rcx,%rdx), %rbx
+	movq	%rax, %rdi
+	call	malloc@PLT
+	movq	%rax, (%rbx)
+	movl	-36(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	testq	%rax, %rax
+	jne	.L21
+	movl	-36(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	movq	-24(%rbp), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	movl	$0, %eax
+	jmp	.L19
+.L21:
+	movl	-36(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	(%rax), %rax
+	movq	-32(%rbp), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	strcpy@PLT
+	addl	$1, -36(%rbp)
+	movq	-64(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$0, %edi
+	call	strtok@PLT
+	movq	%rax, -32(%rbp)
+.L20:
+	cmpq	$0, -32(%rbp)
+	jne	.L22
+	movl	-36(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movq	$0, (%rax)
+	movq	-24(%rbp), %rax
+.L19:
+	addq	$72, %rsp
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9:
+	.size	buff_split, .-buff_split
+	.globl	copy_buffer
+	.type	copy_buffer, @function
+copy_buffer:
+.LFB10:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	-24(%rbp), %rax
+	movq	%rax, %rdi
+	call	_strlen
+	cltq
+	movq	%rax, %rdi
+	call	malloc@PLT
+	movq	%rax, -8(%rbp)
+	cmpq	$0, -8(%rbp)
+	jne	.L24
+	movl	$0, %eax
+	jmp	.L25
+.L24:
+	movq	-24(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	strcpy@PLT
+	movq	-8(%rbp), %rax
+.L25:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE10:
+	.size	copy_buffer, .-copy_buffer
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
